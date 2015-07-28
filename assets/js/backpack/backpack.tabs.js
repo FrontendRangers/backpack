86f373c8e37c2f293ea356067ab5082d7eae88a5
Benoit Deziel <bdeziel@iweb.com>
2015-07-28 14:51:08 -0400

    Updated to version 1.1

--- assets/js/backpack/backpack.tabs.js
+++ assets/js/backpack/backpack.tabs.js
@@ -1,24 +1,42 @@
 /**
 *
 * Backpack Tabs
-* version: 1.0
+* version: 1.1
 *
 **/
 
-(function ($, window, undefined) {
+(function ($) {
 
   "use strict";
 
   $.fn.backpackTabs = function (){
+    var listOfTabs = [];
+    $('.tabs-content .tab-pane').each(function(){
+      listOfTabs.push(this.id);
+    });
+
     return this.each( function() {
       var currentTabs = $(this);
       var currentTabsLinks = $(this).find('.tabs-nav a');
       var currentTabsPane = $(this).find('.tabs-content .tab-pane');
-
-      currentTabsLinks.first().addClass('is-active');
-      currentTabsPane.first().addClass('is-active');
+      var hash = window.location.hash.substr(1);
+      if (!window.location.hash) {
+        currentTabsLinks.first().addClass('is-active');
+        currentTabsPane.first().addClass('is-active');
+        console.log('No hash');
+      } else if ($.inArray(hash, listOfTabs) !== -1) {
+        $('a[data-tab^="'+hash+'"]').addClass('is-active');
+        $('#'+hash).addClass('is-active');
+        console.log('Found in array');
+      } else {
+        currentTabsLinks.first().addClass('is-active');
+        currentTabsPane.first().addClass('is-active');
+        console.log('Not found in array');
+      };
 
       currentTabsLinks.on('click', function (e) {
+        e.preventDefault();
+
         var tab_id = $(this).attr('data-tab');
 
         currentTabsLinks.removeClass('is-active');
@@ -27,7 +45,12 @@
         $(this).addClass('is-active');
         $("#"+tab_id).addClass('is-active');
 
-        e.preventDefault();
+        var offset = $(this).parents('.tabs').offset();
+          if (offset) {
+            $('html, body').animate({ scrollTop: offset.top }, 'slow');
+            return false;
+          }
+
       });
     });
   }
